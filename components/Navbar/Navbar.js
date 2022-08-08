c46d626fd8b0262ce9923db2ee33c80e4a443a61
suhdanny <suhdong21@gmail.com>
2022-08-07 23:22:24 -0400

    Make Navbar response in mobile with media queries

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Navbar.module.css';
 import Image from 'next/image';
+import { RiCloseLine, RiMenu3Line } from 'react-icons/ri';
+
+const Menu = () => {
+	return (
+		<>
+			<p>
+				<a href='#'>about us</a>
+			</p>
+			<p>
+				<a href='#'>what we do</a>
+			</p>
+			<p>
+				<a href='#'>meet the team</a>
+			</p>
+			<p>
+				<a href='#'>articles</a>
+			</p>
+			<p>
+				<a href='#'>contact us</a>
+			</p>
+		</>
+	);
+};
 
 const Navbar = () => {
+	const [toggleMenu, setToggleMenu] = useState(false);
+
 	return (
 		<div className={styles.navbar}>
 			<div className={styles.navbarLinks}>
@@ -10,21 +35,21 @@ const Navbar = () => {
 					<Image src='/images/logo.png' alt='logo' height='75' width='75' />
 				</div>
 				<div className={styles.navbarLinksContainer}>
-					<p>
-						<a href='#'>about us</a>
-					</p>
-					<p>
-						<a href='#'>what we do</a>
-					</p>
-					<p>
-						<a href='#'>meet the team</a>
-					</p>
-					<p>
-						<a href='#'>articles</a>
-					</p>
-					<p>
-						<a href='#'>contact us</a>
-					</p>
+					<Menu />
+				</div>
+				<div className={styles.navbarMenu}>
+					{toggleMenu ? (
+						<RiCloseLine size={27} onClick={() => setToggleMenu(false)} />
+					) : (
+						<RiMenu3Line size={27} onClick={() => setToggleMenu(true)} />
+					)}
+					{toggleMenu && (
+						<div className={`${styles.navbarMenuContainer} ${styles.scaleUpCenter}`}>
+							<div className={styles.navbarMenuContainerLinks}>
+								<Menu />
+							</div>
+						</div>
+					)}
 				</div>
 			</div>
 		</div>
