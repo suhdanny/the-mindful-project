cb635197fa549bd87c9320932c421ddd628330f3
suhdanny <suhdong21@gmail.com>
2022-08-08 22:17:25 -0400

    Add arrow down icon below header section

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './Header.module.css';
 import Image from 'next/image';
 import { loremIpsum } from 'react-lorem-ipsum';
+import { BsChevronCompactDown } from 'react-icons/bs';
 
 const Header = () => {
 	return (
@@ -19,9 +20,12 @@ const Header = () => {
 						avgSentencesPerParagraph: 4,
 					})}
 				</p>
+				<div className={styles.arrowDownIcon}>
+					<BsChevronCompactDown size={35} style={{ color: 'blue' }} />
+				</div>
 			</div>
 			<div className={styles.headerImageContainer}>
-				<Image className={styles.headerImage} src='/images/header.png' alt='header' width={400} height={300} />
+				<Image className={styles.headerImage} src='/images/neuro.jpeg' alt='header' width={450} height={300} />
 			</div>
 		</div>
 	);
