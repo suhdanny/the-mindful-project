c987a77b26b151a6a6bf1a1f351c4ab2a3962846
suhdanny <suhdong21@gmail.com>
2022-08-25 16:09:03 -0400

    Fix About & Header section styling and remove NewsLetter section

--- components/Menu/Menu.tsx
+++ components/Menu/Menu.tsx
@@ -11,11 +11,11 @@ const Menu = () => {
 			<p>
 				<a href='#'>What We Do</a>
 			</p>
-			<p>
+			{/* <p>
 				<Link to='blog' duration={500} spy={true} offset={-100}>
 					News Letter
 				</Link>
-			</p>
+			</p> */}
 			<p>
 				<Link to='team' duration={500} spy={true} offset={-100}>
 					Meet The Team
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Media from '../contents/Media/Media';
 import Navbar from '../contents/Navbar/Navbar';
 import Header from '../contents/Header/Header';
 import About from '../contents/About/About';
-import Blog from '../contents/Blog/Blog';
+// import Blog from '../contents/Blog/Blog';
 import Team from '../contents/Team/Team';
 import Contact from '../contents/Contact/Contact';
 import Footer from '../contents/Footer/Footer';
@@ -19,7 +19,7 @@ const App = () => {
 			<div className='gradient__bg'>
 				<Header />
 				<About />
-				<Blog />
+				{/* <Blog /> */}
 				<Team />
 				<Contact />
 			</div>
