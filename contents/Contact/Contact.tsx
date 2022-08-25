a42b5f95aa95b5d86493476ddc8ed3ce44cb7d86
suhdanny <suhdong21@gmail.com>
2022-08-24 20:18:16 -0400

    Add Google Maps API with env variables

--- contents/Contact/Contact.tsx
+++ contents/Contact/Contact.tsx
@@ -1,9 +1,11 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import emailjs from '@emailjs/browser';
 import styles from './Contact.module.css';
+import { Loader } from '@googlemaps/js-api-loader';
 
 const Contact = () => {
 	const form = useRef<HTMLFormElement>(null);
+	const googleMap = useRef<HTMLDivElement>(null);
 	const [showMessage, setShowMessage] = useState(false);
 
 	const resetForm = () => {
@@ -12,15 +14,42 @@ const Contact = () => {
 
 	const sendEmail = async (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
-
 		try {
-			await emailjs.sendForm('service_m5ga7r4', 'template_fxey18x', form.current, 'rzC9tzp05Gdl9lGdO');
+			await emailjs.sendForm(
+				process.env.NEXT_PUBLIC_SERVICE_ID,
+				process.env.NEXT_PUBLIC_TEMPLATE_ID,
+				form.current,
+				process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
+			);
 			resetForm();
 			setShowMessage(true);
 		} catch (error) {
 			console.log(error);
 		}
 	};
+
+	useEffect(() => {
+		const loader = new Loader({
+			apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+			version: 'weekly',
+		});
+
+		const university = { lat: 43.653, lng: -79.399 };
+
+		let map: google.maps.Map;
+
+		loader.load().then(() => {
+			map = new google.maps.Map(googleMap.current, {
+				center: university,
+				zoom: 12,
+			});
+
+			const marker = new google.maps.Marker({
+				position: university,
+				map: map,
+			});
+		});
+	}, []);
 
 	return (
 		<div className={styles.contact} id='contact'>
@@ -38,7 +67,9 @@ const Contact = () => {
 					<textarea name='message' required />
 					<input type='submit' value='Send' />
 				</form>
-				<div className={styles.contactImage}></div>
+				<div className={styles.contactMap}>
+					<div className={styles.googleMap} ref={googleMap} />
+				</div>
 			</div>
 		</div>
 	);
