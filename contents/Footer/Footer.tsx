c62e117a36ad40d06ad3854294068597640a7131
suhdanny <suhdong21@gmail.com>
2022-08-28 21:46:55 -0400

    Show social media icons in the footer when <700px

--- contents/Footer/Footer.tsx
+++ contents/Footer/Footer.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import styles from './Footer.module.css';
 import Menu from '../../components/Menu/Menu';
+import { FaInstagram, FaFacebookF, FaLinkedinIn } from 'react-icons/fa';
+import { useMediaQuery } from '../../hooks/useMediaQuery';
 
 const Footer = () => {
+	const showMediaIcons = useMediaQuery('(max-width: 700px)');
+
 	return (
 		<div className={styles.footer}>
 			<div className={styles.footerLinks}>
@@ -26,6 +30,22 @@ const Footer = () => {
 					<p className={styles.footerMenuHeader}>Emails</p>
 					<p>uoftthemindfulproject@gmail.com</p>
 				</div>
+				{showMediaIcons && (
+					<div className={styles.footerMenu}>
+						<p className={styles.footerMenuHeader}>Social Media</p>
+						<div className={styles.mediaContainer}>
+							<a href='https://www.instagram.com/uoftthemindfulproject/' target='_blank' rel='noopener noreferrer'>
+								<FaInstagram size={20} style={{ cursor: 'pointer' }} />
+							</a>
+							<a href='https://www.facebook.com/uoftthemindfulproject' target='_blank' rel='noopener noreferrer'>
+								<FaFacebookF size={20} style={{ cursor: 'pointer' }} />
+							</a>
+							<a href='https://www.linkedin.com/company/u-of-t-the-mindful-project/' target='_blank' rel='noopener noreferrer'>
+								<FaLinkedinIn size={20} style={{ cursor: 'pointer' }} />
+							</a>
+						</div>
+					</div>
+				)}
 			</div>
 			<div className={styles.footerCopyRight}>© University of Toronto The Mindful Project 2022</div>
 		</div>
